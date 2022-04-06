8ad24f751605dcf98cda1c284cc80b5b970f54a7
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-04-06 10:04:30 +0200

    Adding country filtering to get_giftcard_products.sh

--- get_giftcard_products.sh
+++ get_giftcard_products.sh
@@ -2,17 +2,23 @@
 
 if [ "$#" -lt 1 ]
 then
-	echo "Please specify Environment (prod|test)"
+	echo "Please specify Environment (prod|test), optionally specify country ISO Code to filter"
 
 	exit -1	
 fi
 
 ENV="$1"
-
 if [ "test" = "$ENV" ]
 then
 	SANDBOX="-sandbox"
 fi
+
+URL='https://giftcards'$SANDBOX'.reloadly.com/products' 
+if [ "$#" -ge 2 ]
+then
+	URL='https://giftcards'$SANDBOX'.reloadly.com/countries/'$2'/products' 
+fi
+
 
 TOKEN_FILE="giftcards.token.$ENV"
 
@@ -25,7 +31,7 @@ fi
 >&2 echo "Using token from $TOKEN_FILE"
 TOKEN=`cat $TOKEN_FILE`
 
-curl --location --request GET 'https://giftcards'$SANDBOX'.reloadly.com/products' \
+curl --location --request GET $URL \
 	--header 'Authorization: Bearer '$TOKEN \
 	--header 'Accept: application/com.reloadly.giftcards-v1+json' \
-	--header 'Content-Type: application/json' | jq $2 
+	--header 'Content-Type: application/json'  
